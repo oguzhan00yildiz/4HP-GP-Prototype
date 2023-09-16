246a16824f6dc9346a981b0a2fe445fb998072df
Ville Ruokonen <ruokonenv@gmail.com>
2023-09-16 20:07:24 +0300

    Reworked player movement
    + changed transform translation to rigidbody movement
    - removed separate CameraFollowTarget script

---
+++ Assets/Resources/Scripts/Miscellaneous/LerpToTarget.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class LerpToTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _lerpSpeed;
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
+    public void SetSpeed(float speed)
+    { 
+        _lerpSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_target == null)
+            return;
+
+        Vector3 curPos = transform.position;
+
+        // set z to remain the same as current position z so camera doesn't go forward/backward in 3D space
+        Vector3 targetPos = new Vector3()
+        {
+            x = _target.position.x,
+            y = _target.position.y,
+            z = curPos.z
+        };
+
+        transform.position = Vector3.Lerp(curPos, targetPos, _lerpSpeed * Time.deltaTime);
+    }
+}
--- Assets/Resources/Scripts/Player/PlayerMovement.cs
+++ Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
     const float WALK_SPEED = 5.0f;
     private Vector2 _nextMovement;
+    private Rigidbody2D _rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
 
+        if(_rb == null)
+        {
+            Debug.LogWarning("Player rigidbody is null, you stupid idiot!!");
+        }
     }
 
     // Update is called once per frame
@@ -41,15 +47,20 @@ public class PlayerMovement : MonoBehaviour
         // Lerp movement towards 0 if the player is not desiring to move (smoother stop)
         if(input.magnitude <= 0)
         {
-            _nextMovement = Vector2.Lerp(_nextMovement, Vector2.zero, Time.deltaTime * 25.0f);
+            _nextMovement = Vector2.Lerp(_nextMovement, Vector2.zero, Time.deltaTime * 15.0f);
         }
         else
         {
             // Else lerp the next movement towards the current desired movement (smooth acceleration)
-            _nextMovement = Vector2.Lerp(_nextMovement, screenMovement, Time.deltaTime * 50.0f);
+            _nextMovement = Vector2.Lerp(_nextMovement, screenMovement, Time.deltaTime * 25.0f);
         }
 
-        // Move (note: it would be better to do this in FixedUpdate so movement happens during physics checks
-        transform.Translate(_nextMovement * Time.deltaTime);
+        // Movement updates happen in FixedUpdate since Unity physics run in that timeframe
+    }
+
+    private void FixedUpdate()
+    {
+        // Cast to Vector2 required for transform.position change
+        _rb.MovePosition((Vector2)transform.position + _nextMovement * Time.fixedDeltaTime);
     }
 }
