9cd3d5386b614378c77f9e19debbb20a233c9824
Ville Ruokonen <ruokonenv@gmail.com>
2023-09-23 21:32:08 +0300

    Enemy rigidbody movement
    + added enemy movement and stopping distance from target (not sure if working)

--- Assets/Resources/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Resources/Scripts/Enemies/EnemyMovement.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _stoppingDistance = 2.0f;
+    [SerializeField] private Rigidbody2D _rigidbody;
 
     private Transform _target;
+    private Vector3 _targetDirection;
 
     private void Start()
     {
         _target = FindObjectOfType<Player>().transform;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        if(_rigidbody == null)
+        {
+            _rigidbody = gameObject.AddComponent<Rigidbody2D>();
+            _rigidbody.gravityScale = 0.0f;
+            _rigidbody.freezeRotation = true;
+        }
     }
 
     private void Update()
     {
         if (_target != null) 
         {
-            Vector3 direction = (_target.position - transform.position).normalized;
-            transform.Translate(direction * Time.deltaTime * _speed);
+            // Not normalized since we'll use magnitude to gauge distance
+            Vector3 direction = (_target.position - transform.position);
+            _targetDirection = direction;
+
+            // Move in fixed update
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_targetDirection.magnitude > _stoppingDistance)
+        {
+            Vector3 moveAmount = transform.position + _targetDirection.normalized * _speed * Time.fixedDeltaTime;
+            _rigidbody.MovePosition(moveAmount);
         }
     }
 
