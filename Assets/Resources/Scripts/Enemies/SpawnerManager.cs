0d7785997288e6dc1817f5dfbe79bf4b92d20a91
TrennaGiacomo <giacomo.trenna06@gmail.com>
2023-09-24 20:48:47 +0300

    Added percentage check for each spawned enemy

--- Assets/Resources/Scripts/Enemies/SpawnerManager.cs
+++ Assets/Resources/Scripts/Enemies/SpawnerManager.cs
@@ -14,7 +14,7 @@ namespace Assets.Resources.Scripts.Enemies
         private List<GameObject> _enemiesToSpawn = new();
         private Transform _player;
         private float _nextSpawnTime;
-        private int _waveValue;
+        private int _waveBudget;
 
         private const float SpawnRadius = 15;
         private const float EnemiesPerSecond = 2;
@@ -58,17 +58,18 @@ namespace Assets.Resources.Scripts.Enemies
         // This method decides the "budget" for the wave
         private void CalculateWaveBudget() 
         {
-            _waveValue = currentWave * 10;
+            _waveBudget = currentWave * 10;
             GenerateEnemies();
         }
 
         // Creates the list of enemies that then are going to be spawned according to current wave and budget
         private void GenerateEnemies()
         {
-            // Creates a list of all the unlocked enemies given the current wave
+            // LINQ expression to creates a list of all the unlocked enemies given the current wave
             var availableEnemies = _enemies.Where(enemy => enemy.unlockingWave <= currentWave && currentWave <= enemy.lastSpawningWave).ToList();
             
             List<GameObject> generatedEnemies = new();
+            var remainingBudget = _waveBudget;
 
             // Chooses enemies at random from the available ones, until there are no more enemies available or enemies cap reached
             while (availableEnemies.Count != 0 && _enemiesToSpawn.Count < MaxEnemiesPerRound)
@@ -76,17 +77,26 @@ namespace Assets.Resources.Scripts.Enemies
                 var randEnemyId = Random.Range(0, availableEnemies.Count);
                 var randEnemyCost = availableEnemies[randEnemyId].enemyCost;
 
-                if (_waveValue - randEnemyCost > 0)
+                if (remainingBudget - randEnemyCost > 0)
                 {
-                    generatedEnemies.Add(availableEnemies[randEnemyId].enemyPrefab);
-                    _waveValue -= randEnemyCost;
+                    // Finds the number of the specific enemy that have already been added to the list and calculates the percentage compared to the whole budget for the wave
+                    var specificEnemyPercentage = ((float)generatedEnemies.FindAll(obj => availableEnemies[randEnemyId].enemyPrefab).Count) / _waveBudget * 100;
+
+                    // Check if there are already too many of that enemy in the wave, if not, it adds it to the list, if yes, it deletes it from the available enemies
+                    if (specificEnemyPercentage < availableEnemies[randEnemyId].maxPercentageInWave)
+                    {
+                        generatedEnemies.Add(availableEnemies[randEnemyId].enemyPrefab);
+                        remainingBudget -= randEnemyCost;
+                    }
+                    else
+                        availableEnemies.Remove(availableEnemies[randEnemyId]);
                 }
                 else
                  availableEnemies.Remove(availableEnemies[randEnemyId]);
             }
 
             _enemiesToSpawn.Clear();
-            _enemiesToSpawn = generatedEnemies;
+            _enemiesToSpawn = generatedEnemies;     
         }
 
         // This method returns a random spawn point so for SpawnEnemy() to use
--- Assets/Resources/Scripts/Global/GameManager.cs
+++ Assets/Resources/Scripts/Global/GameManager.cs
@@ -7,21 +7,19 @@ namespace Assets.Resources.Scripts.Global
     public class GameManager : MonoBehaviour
     {
         #region Singleton
-        private static GameManager instance;
+        private static GameManager _instance;
         public static GameManager Instance
         {
             get
             {
-                if (instance == null)
-                {
-                    instance = FindObjectOfType<GameManager>();
-                    if (instance == null)
-                    {
-                        GameObject obj = new GameObject("GameManager");
-                        instance = obj.AddComponent<GameManager>();
-                    }
-                }
-                return instance;
+                if (_instance != null) return _instance;
+                _instance = FindObjectOfType<GameManager>();
+
+                if (_instance != null) return _instance;
+                var obj = new GameObject("GameManager");
+                _instance = obj.AddComponent<GameManager>();
+
+                return _instance;
             }
         }
         #endregion
@@ -31,9 +29,9 @@ namespace Assets.Resources.Scripts.Global
 
         private void Awake()
         {
-            if (instance == null)
+            if (_instance == null)
             {
-                instance = this;
+                _instance = this;
                 DontDestroyOnLoad(gameObject);
             }
             else
@@ -61,7 +59,7 @@ namespace Assets.Resources.Scripts.Global
         }
 
         // Updates the health bar to the new value with a smooth animation
-        private IEnumerator UpdateHealthBar(EnemyData enemyHit, int initialHealth, float targetHealthRatio)
+        private static IEnumerator UpdateHealthBar(EnemyData enemyHit, int initialHealth, float targetHealthRatio)
         {
             ;
             var elapsedTime = 0f;
